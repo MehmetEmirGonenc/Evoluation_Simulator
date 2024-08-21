b02986e1ec555db774a770570570efd886d0159d
Mehmet Gönenç <mehmetemirgonenc@gmail.com>
2024-08-21 14:01:11 +0300

    Changed main

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 # Importing classes
 from creator import Creator
 from event import Event
+from interface import *
 
 def main():
+    
+    """    creature_list = []
     
     c1 = Creator("Man")
     c1.add_features ([["Hair", False]])
     c1.change_feature("Hair", True)
     c1.print_features()
-    
+    creature_list.append(c1)"""
+    """    
     e1 = Event("Test", c1, 2)
     e1.set_probablity(40)
     e1.set_trigger("health", 50, "health", 'G', 5)
@@ -17,5 +21,8 @@ def main():
     c1.print_features()
     e1.set_trigger("health", 50, "health", 'G', 5)
     c1.print_features()
-
+    """
+    
+    call_main_interface()
+    
 main()
